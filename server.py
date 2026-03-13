1aefe83e05608bd92d1ea75e1a41f86da9e4f46a
Calvin <calvin.roth04@gmail.com>
2026-03-13 13:41:31 +0100

    Changed rng

--- server.py
+++ server.py
@@ -9,10 +9,38 @@ app = Flask(__name__)
 agents = {}
 
 def randomuuid():
-    int1 = random.randint(1000, 9999)
-    int2 = random.randint(1000, 9999)
-    int3 = random.randint(1000, 9999)
-    int4 = random.randint(1000, 9999)
+    int1 = random.randint(1, 9999)
+    if int1 < 10:
+        int1 = "000" + str(int1)
+    elif int1 < 100:
+        int1 = "00" + str(int1)
+    elif int1 < 1000:
+        int1 = "0" + str(int1)
+
+    int2 = random.randint(1, 9999)
+    if int2 < 10:
+        int2 = "000" + str(int2)
+    elif int2 < 100:
+        int2 = "00" + str(int2)
+    elif int2 < 1000:
+        int2 = "0" + str(int2)
+
+    int3 = random.randint(1, 9999)
+    if int3 < 10:
+        int3 = "000" + str(int3)
+    elif int3 < 100:
+        int3 = "00" + str(int3)
+    elif int3 < 1000:
+        int3 = "0" + str(int3)
+
+    int4 = random.randint(1, 9999)
+    if int4 < 10:
+        int4 = "000" + str(int4)
+    elif int4 < 100:
+        int4 = "00" + str(int4)
+    elif int4 < 1000:
+        int4 = "0" + str(int4)
+
     int_random = str(int1) + "-" + str(int2) + "-" + str(int3) + "-" + str(int4)
     return int_random
 
@@ -25,7 +53,6 @@ def register():
     guid = randomuuid()
     if guid not in agents:
         agents[guid] = {
-        "id": guid,
         "ip": request.remote_addr }
         return guid
     else:
@@ -54,12 +81,9 @@ def output(agent_guid):
     if agent_guid not in agents:
         return "No such agent"
     else:
-        data = request.get_json()
+        data = request.get_data(as_text=True)
 
-        with open(f"results_{agent_guid}.txt", "a") as f:
-            f.write(data["output"] + "\n")
-
-        return "OK"
+        agents[agent_guid]["output"] = agents[agent_guid].get("output", "") + data + "\n"
 
 
 @app.route("/agent/list")
